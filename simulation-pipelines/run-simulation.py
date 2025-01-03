344f29246047815a48c8903f4a9568e5df2d1b3e
Nicolas Cecchi <nicolas.cecchi1@ens-paris-saclay.fr>
2025-01-03 11:49:38 +0100

    Change saving folder name for ease of notation

--- simulation-pipelines/run-simulation.py
+++ simulation-pipelines/run-simulation.py
@@ -7,7 +7,7 @@ import os
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument("algo", help="Algorithm to run simulation. One of [sop, tf, smsp]")
+parser.add_argument("algo", help="Algorithm to run simulation. One of [c-spop, p-spop, tf, smsp]")
 parser.add_argument("npoints", help="Number of points in the signal to load.", type=int)
 parser.add_argument(
     "--savefolder", help="Directory where to save the results.", default=None
@@ -18,24 +18,27 @@ if args.savefolder is None:
     savefolder = (
         str(datetime.datetime.today())
         .replace(" ", "-")
-        .replace(":", "-")
-        .replace(".", "-")
+        .replace(":", "-")[0:16]
     )
     savefolder = args.algo + "-" + str(args.npoints) + "-" + savefolder
     args.savefolder = savefolder
 if __name__ == "__main__":
     match args.algo:
-        case "sop":
-            import runSplineop
-
-            runSplineop.main(args.npoints, args.savefolder)
+        case "c-spop":
+            import runConstrainedSplineop
+            runConstrainedSplineop.main(args.npoints, args.savefolder)
+        
+        case "p-spop":
+            import runPenalizedSplineop
+            runPenalizedSplineop.main(args.npoints, args.savefolder)
+        
         case "tf":
             import runTrendFiltering
-
             runTrendFiltering.main(args.npoints, args.savefolder)
+        
         case "smsp":
             import runSmoothingSpline
-
             runSmoothingSpline.main(args.npoints, args.savefolder)
+        
         case _:
             raise ValueError("Unsupported algorithm. Check spelling. ")
