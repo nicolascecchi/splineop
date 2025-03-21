f9b0254a21e223ed8a3c4f7cc27718de024e0c51
Nicolas Cecchi <nicolas.cecchi1@ens-paris-saclay.fr>
2025-03-21 09:27:57 +0100

    Update generate_states for a multi-dimensional signal

--- splineop/splineop.py
+++ splineop/splineop.py
@@ -646,35 +646,50 @@ def compute_speeds_from_multi_obs(y,pcts=[0.5, 1, 1.5, 2, 2.5])-> np.array:
     speeds = np.apply_along_axis(compute_speeds_from_observations,1,y,pcts)
     return speeds
 
-def state_generator(yobs,n_states=21, pct=0.05, local=True):
+def state_generator(signal,n_states=5, pct=0.05, local=True):
     """
     Generates a grid of states around the observations. 
 
     Parameters:
-    yobs (np.array) : Array of observations
+    signal (np.array) : Array of observations
     n_states (int) : Number of states to fit.
     pct (float) : Percentage of the value range to be taken to form the states-grid.
     
     Returns:
     states (np.NDarray) : (n_obs+1, n_states)-shaped array with the states for each observation.
     """
-    nsignals = len(yobs)
-    m = len(yobs)
-    output = np.zeros((m+1, n_states))
-    
-    # Compute the inverval around the points
-    max_signal = np.max(yobs)
-    min_signal = np.min(yobs)
-    interval = np.abs(max_signal - min_signal)
-    delta = interval * pct
-    if local:
-        for i in range(m):
-            start = yobs[i] - delta/2
-            end = yobs[i] + delta/2
-            output[i] = np.linspace(start,end, n_states, True)
-        output[-1] = output[-2]
+    if signal.shape[0]== 1:
+        m = len(signal)
+        states = np.zeros((m+1, n_states))
+        
+        # Compute the inverval around the points
+        max_signal = np.max(signal)
+        min_signal = np.min(signal)
+        interval = np.abs(max_signal - min_signal)
+        delta = interval * pct
+        if local:
+            for i in range(m):
+                start = signal[i] - delta/2
+                end = signal[i] + delta/2
+                states[i] = np.linspace(start,end, n_states, True)
+            states[-1] = states[-2]
+        else:
+            for i in range(m):
+                states[i] = np.linspace(min_signal,max_signal, n_states, True)
+            states[-1] = states[-2]
+        return states
     else:
-        for i in range(m):
-            output[i] = np.linspace(min_signal,max_signal, n_states, True)
-        output[-1] = output[-2]
-    return output
+        each_side = (n_states - 1) // 2
+
+        signal_length, signal_dims = signal.shape
+        states_shape = (signal_length, n_states, signal_dims)
+        states = np.zeros(shape=states_shape)
+        print( signal_length - each_side - 1)
+        for i in range(each_side, signal_length - each_side - 1):
+            
+            states[i] = signal[i-each_side:i+each_side + 1]
+        for i in range(0,each_side):
+            states[i] = signal[0:n_states]
+            states[signal_length - (each_side+1)  + i] = signal[-n_states:]
+        states[-1] = signal[-n_states:]
+        return states
