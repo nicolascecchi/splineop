6b46e72369727162928c8a7ad6086064cc5e714e
Nicolas Cecchi <nicolas.cecchi1@ens-paris-saclay.fr>
2025-07-18 14:21:45 +0200

    Remove state generator from module. Remove unused imports.

--- splineop/splineop.py
+++ splineop/splineop.py
@@ -3,13 +3,10 @@ from numba import njit, objmode
 from numba.experimental import jitclass
 from numba import int64, float64, int64, float64
 from scipy import interpolate
-from scipy.stats import dirichlet
 import matplotlib.pyplot as plt
 from splineop.costs import *
-import pdb
 from timeit import default_timer as timer
 from sklearn.linear_model import LinearRegression
-from splineop.sputils import sd_hall_diff
 
 
 splineop_spec_Pen = [("cost", costPenalized.class_type.instance_type)]
@@ -779,79 +776,3 @@ def compute_speeds_from_multi_obs(y
         speeds = np.apply_along_axis(compute_speeds_from_observations, 1, y, end_indexes=end_indexes)
     return speeds
 
-
-def state_generator(signal, n_states=5, pct=0.05, local=True):
-    """
-    Generates a grid of states around the observations.
-
-    Parameters:
-    signal (np.array) : Array of observations
-    n_states (int) : Number of states to fit.
-    pct (float) : Percentage of the value range to be taken to form the states-grid.
-
-    Returns:
-    states (np.NDarray) : (n_obs+1, n_states)-shaped array with the states for each observation.
-    
-    If the signal is 1-Dimensional, create points around each observation based on the amplitud
-    of the observations. 
-    "Local" hyperparam makes the states around the observed points.
-    
-    If the signal d-Dimensional, the states are taken to be each_side/2 points before and each_side/2 ahead
-    of each observation. For the first/last few points, where this is not possible because you run out of signal,
-    simply repeat the first (or last) n_states points of the signal.
-    We have to do this because discretizing aroung each dimension independtly would lead again to
-    combinatorial explosion. 
-    """
-    if signal.shape[1] == 1:
-        m = len(signal)
-        states = np.zeros((m + 1, n_states))
-
-        # Compute the inverval around the points
-        max_signal = np.max(signal)
-        min_signal = np.min(signal)
-        interval = np.abs(max_signal - min_signal)
-        delta = interval * pct
-        if local:
-            for i in range(m):
-                start = signal[i] - delta / 2
-                end = signal[i] + delta / 2
-                states[i] = np.linspace(start[0], end[0], n_states, True)
-            states[-1] = states[-2]
-        else:
-            for i in range(m):
-                states[i] = np.linspace(min_signal, max_signal, n_states, True)
-            states[-1] = states[-2]
-        states = np.expand_dims(states, 2) # Ensures dimension as (n_samples+1, n_states, n_dims)
-    else:
-        each_side = (n_states - 1) // 2            
-        try:
-            signal_length, signal_dims = signal.shape
-        except:
-            signal_length, signal_dims = signal.shape[0], 1
-            signal = signal.reshape(signal_length, signal_dims)
-        states_shape = (signal_length + 1, n_states, signal_dims)
-        states = np.zeros(shape=states_shape)
-        for i in range(each_side, signal_length - each_side):
-            states[i] = signal[i - each_side : i + each_side + 1]
-        for i in range(0, each_side):
-            states[i] = signal[i:n_states+i]
-            states[signal_length - each_side + i] = signal[-n_states:]
-        states[-1] = signal[-n_states:]
-
-    return states
-
-def state_generator_v2(signal:np.array, n_states:int=5, local:bool=True):
-    try:
-        signal_length, signal_dims = signal.shape
-    except:
-        signal_length, signal_dims = signal.shape[0], 1
-    signal = signal.reshape(signal_length, signal_dims)
-    states_shape = (signal_length, n_states, signal_dims)
-    states = np.zeros(shape=states_shape)
-    #states[-1] = signal[-1]
-    varlist = sd_hall_diff(signal,var=False)
-    for dim in range(signal_dims):
-        noise = np.random.normal(0, varlist[dim], (signal_length,n_states))
-        states[:,:,dim] = signal[:,dim,None] + noise
-    states =  np.concat((states, states[-1,:,:][None]))
-    return states
